17217a1c383b7ddde8e0fc5452a09026997451af
DenysKors <105918404+DenysKors@users.noreply.github.com>
2023-12-01 15:53:31 +0200

    Pagination

--- src/pages/Todos/Todos.jsx
+++ src/pages/Todos/Todos.jsx
@@ -2,16 +2,38 @@ import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
+import Pagination from "@mui/material/Pagination";
 
 import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 import { TodoCounter } from "../../components/TodoCounter/TodoCounter";
 import { TodoItem } from "../../components/TodoItem/TodoItem";
 import { StatusFilter } from "../../components/StatusFilter/StatusFilter";
 import { selectVisibleTodos } from "../../redux/todos/selectors";
 
+const todosPerPage = 10;
+
 function Todos() {
+	const [page, setPage] = useState(1);
+	const [item, setItem] = useState(0);
+	const [currentTodos, setCurrentTodos] = useState([]);
+
 	const todos = useSelector(selectVisibleTodos);
+
+	const amountOfPages = Math.ceil(todos.length / todosPerPage);
+
+	useEffect(() => {
+		const endOffset = item + todosPerPage;
+		const paginationItems = todos.slice(item, endOffset);
+		setCurrentTodos(paginationItems);
+	}, [todos, item]);
+
+	const handleChange = (_, value) => {
+		setPage(value);
+		setItem(value * todosPerPage - todosPerPage);
+	};
+
 	return (
 		<>
 			<Toolbar />
@@ -28,8 +50,13 @@ function Todos() {
 					<StatusFilter />
 				</Box>
 				<List>
-					{todos?.length > 0 ? todos.map(todo => <TodoItem key={todo.id} todo={todo} />) : <p>Nothing found</p>}
+					{todos?.length > 0 ? currentTodos.map(todo => <TodoItem key={todo.id} todo={todo} />) : <p>Nothing found</p>}
 				</List>
+				{todos?.length > 0 && (
+					<Box sx={{ mt: 2, display: "flex", justifyContent: "center" }}>
+						<Pagination count={amountOfPages} page={page} onChange={handleChange} variant="outlined" color="primary" />
+					</Box>
+				)}
 			</Container>
 		</>
 	);
